3fa0305342d927107e168800e05a9d069a7b9c0f
Czechu7 <choocas0@gmail.com>
2024-11-19 12:10:22 +0100

    add all

---
+++ NASA.API/NASA.API/API/Data/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext // Poprawiona składnia
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) // Typ DbContextOptions
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Decisions> Decisions { get; set; }
+        public DbSet<Question> Question { get; set; } 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Konfiguracja relacji jeden-do-wielu
+            modelBuilder.Entity<Question>()
+                .HasMany(q => q.Decisions)
+                .WithOne(d => d.Question)
+                .HasForeignKey(d => d.QuestionId)
+                .OnDelete(DeleteBehavior.Cascade); // Ustala, co się stanie, gdy pytanie zostanie usunięte
+
+        modelBuilder.Entity<AppUser>()
+        .Property(u => u.Id)
+        .ValueGeneratedOnAdd();
+        }
+    }
+}
